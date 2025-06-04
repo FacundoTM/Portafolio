f4af341fbc04ca1030c9af8c74cc55b5ed53d3df
Facundo Marchionna <tmfacundo@gmail.com>
2025-06-03 22:25:01 -0300

    Solucionado error de js

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import "preline";
 
-window.addEventListener("load", () => {
-  window.HSStaticMethods?.autoInit?.();
-});
+useEffect(() => {
+  window.addEventListener("load", () => {
+    window.HSStaticMethods?.autoInit?.();
+  });
+}, []);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
