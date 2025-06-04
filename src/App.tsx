37c5872c3dbdd39f78be5256f35e1c63dcbf8092
Facundo Marchionna <tmfacundo@gmail.com>
2025-06-04 00:45:08 -0300

    Arreglado problema finalmente.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import { useEffect } from "react";
 import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Tech from "./components/Tech";
-import "preline";
-
-declare global {
-  interface Window {
-    HS?: {
-      init?: () => void;
-      [key: string]: any;
-    };
-  }
-}
 
 function App() {
-  useEffect(() => {
-    document.addEventListener("DOMContentLoaded", function () {
-      window.HS?.init?.();
-    });
-  }, []);
   return (
     <>
       <div className="container mx-auto sm:py-4">
