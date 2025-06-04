3a0eeed6b363388f6deb4de6ea07d4b5cdc31aa5
Facundo Marchionna <tmfacundo@gmail.com>
2025-06-03 22:08:38 -0300

    Solucionado error de main.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,8 @@
-<<<<<<< HEAD
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import "preline";
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import 'preline'
-
-window.addEventListener('load', () => {
-  window.HSStaticMethods?.autoInit?.()
-})
->>>>>>> f71830054a1a8c7ca8355c53c2e57ec4bf61cdd3
 
 window.addEventListener("load", () => {
   window.HSStaticMethods?.autoInit?.();
