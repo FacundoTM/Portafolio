36c7c929aece66856544ff9616302d0da1f89621
Facundo Marchionna <tmfacundo@gmail.com>
2025-06-04 00:01:34 -0300

    Solucionando error de useEffect

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,7 @@
-import { StrictMode, useEffect } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import "preline";
-
-declare global {
-  interface Window {
-    HS?: {
-      init?: () => void;
-      [key: string]: any;
-    };
-  }
-}
-
-useEffect(() => {
-  document.addEventListener("DOMContentLoaded", function () {
-    window.HS?.init?.();
-  });
-}, []);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
